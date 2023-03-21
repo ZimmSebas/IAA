e1d5f760bb4450f1d31f98e1ed4c458a77a54fdc
ZimmSebas <zimmermannsebas@gmail.com>
2023-03-20 23:13:31 -0300

    Adding non-working version for TP0 with pandas and data generation, missing plotting

--- TP_0/data_gen.py
+++ TP_0/data_gen.py
@@ -1,30 +1,6 @@
-import math
-import sys
-
-
-def std_dev(d, c):
-    return c * math.sqrt(d)
-
-
-def lectura_datos():
-    d = int(input("Ingrese valor entero d\n"))
-    n = int(input("Ingrese valor entero n\n"))
-    c = float(input("Ingrese valor real C:\n"))
-
-    print("Ingrese d puntos d dimensionales, con sus valores separados por espacios")
-
-    puntos = []
-
-    for i in range(d):
-        s = input()
-        s = s.split()
-
-        if len(s) != d:
-            print("Error tamaño distinto de d")
-            continue
-        puntos.append(s)
-
-    return (d, n, c, puntos)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mpl
 
 
 def centros_ej1(d):
@@ -53,6 +29,53 @@ def centros_ej2(d):
     return (c_1, c_2)
 
 
-def elige_clase(p, c_1, c_2):
-    # Deberia elegir a qué clase corresponde cada punto. Si supiera como
-    return
+def generar_valores(centros, c, d, n):
+    (c_1, c_2) = centros
+    std_dev = c**2
+    dev_mat = np.diag([std_dev] * d)
+    values_centro_1 = (
+        np.random.default_rng()
+        .multivariate_normal(mean=c_1, cov=dev_mat, size=n // 2)
+        .tolist()
+    )
+    values_centro_2 = (
+        np.random.default_rng()
+        .multivariate_normal(mean=c_2, cov=dev_mat, size=n // 2)
+        .tolist()
+    )
+    values = (values_centro_1, values_centro_2)
+    dataframe = pd.DataFrame(values)
+    return dataframe
+
+
+def plot(df):
+    print(df[0])
+    mpl.scatter(df[0], "r+", label="Clase 0")
+    mpl.scatter(df[1], "bo", label="Clase 1")
+    mpl.show()
+
+
+def pretest():
+    d = 2
+    n = 8
+    c = 0.75
+    centros = centros_ej1(d)
+    dataf = generar_valores(centros, c, d, n)
+    plot(dataf)
+
+
+def test_ej_a():
+    d = 2
+    n = 200
+    c = 0.75
+    centros = centros_ej1(d)
+    (valores_clase_1, valores_clase_2) = generar_valores(centros, c, d, n)
+    plot(valores_clase_1, valores_clase_2)
+
+
+def test_ej_b():
+    d = 2
+    n = 200
+    centros = centros_ej2(d)
+    (valores_clase_1, valores_clase_2) = generar_valores(centros, c, d, n)
+    plot(valores_clase_1, valores_clase_2)
