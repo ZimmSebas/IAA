6897ec0ea6930824409bbb49e5d90657acca6818
ZimmSebas <zimmermannsebas@gmail.com>
2023-06-25 20:08:32 -0300

    First steps (non working) of exercise 2

--- knn.py
+++ knn.py
@@ -145,6 +145,83 @@ def ejercicio_1():
 
   ejercicio_1_noise(data_noisy, test_noisy)
 
+def knn_train(n, X_train, y_train, X_val, y_val, X_test, y_test):
+  knclf = KNeighborsClassifier(n)
+
+  knclf.fit(X_train, np.ravel(y_train))
+
+  results_test = knclf.predict(X_test)
+  results_val = knclf.predict(X_val)
+  results_train = knclf.predict(X_train)
+
+  error_train = 1 - accuracy_score(y_train, results_train)
+  error_val = 1 - accuracy_score(y_val, results_val)
+  error_test = 1 - accuracy_score(y_test, results_test)
+
+  return error_train, error_val, error_test
+
 def ejercicio_2():
-   return 
-  
+  c = 0.78
+  n = 250
+
+  d_values = [2, 4, 8, 16, 32]
+
+  errors = []
+
+  for d in d_values:
+      centros_a = centros_eja(d)
+
+      test_case_a = generar_valores(centros_a, c * sqrt(d), d, 10000)
+      X_test, y_test = test_case_a.iloc[:, :-1], test_case_a.iloc[:, -1:]
+
+      test_error_para = 0.0
+      values_error_para = 0.0
+      test_error_diag = 0.0
+      values_error_diag = 0.0
+
+      for j in range(20):
+          values = generar_valores(centros_a, c * sqrt(d), d, n)
+          X_train, y_train = values.iloc[:, :-1], values.iloc[:, -1:]
+
+          v_errors, t_errors = knn_train(X_train, y_train, X_val, y_val, X_test, y_test)
+
+          test_error_para += t_errors
+          values_error_para += v_errors
+
+      test_error_para = test_error_para / 20
+      values_error_para = values_error_para / 20
+
+      centros_b = centros_ejb(d)
+      test_case_b = generar_valores(centros_b, c, d, 10000)
+      X_test, y_test = test_case_b.iloc[:, :-1], test_case_b.iloc[:, -1:]
+
+      for j in range(20):
+          values = generar_valores(centros_b, c, d, n)
+          X_train, y_train = values.iloc[:, :-1], values.iloc[:, -1:]
+
+          v_errors, t_errors = knn_train(X_train, y_train, X_test, y_test)
+
+          test_error_diag += t_errors
+          values_error_diag += v_errors
+
+      test_error_diag = test_error_diag / 20
+      values_error_diag = values_error_diag / 20
+
+      errors.append([test_error_para, d, "Test_Parallel_BY"])
+      errors.append([values_error_para, d, "Val_Parallel_BY"])
+      errors.append([test_error_diag, d, "Test_Diagonal_BY"])
+      errors.append([values_error_diag, d, "Val_Diagonal_BY"])
+
+  df_errors = pd.DataFrame(errors, columns=["Error", "D", "Type"])
+  df_errors.to_csv("TP_4/errors_ej_2.csv", index=False)
+
+
+def ejercicio_2_print():
+    df_errors_tree = pd.read_csv("TP_1/errors_ej_4.csv")
+    df_errors_nn = pd.read_csv("TP_2/errors_ej_5.csv")
+    df_errors_bayes = pd.read_csv("TP_3/errors_ej_1.csv")
+    df_errors_knn = pd.read_csv("TP_4/errors_ej_2.csv")
+    df_errors = pd.concat([df_errors_tree, df_errors_nn, df_errors_bayes, df_errors_knn])
+    print(df_errors)
+    plot_error_lines_with_dimensions(df_errors)
+
