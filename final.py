63f60228de5ac265923c2203a0147aced6952876
ZimmSebas <zimmermannsebas@gmail.com>
2023-07-24 08:40:38 +0200

    Adding exercise 1-2

--- final.py
+++ final.py
@@ -1,7 +1,32 @@
 import tensorflow as tf 
 from tensorflow.keras import datasets, layers, models
 from sklearn.model_selection import train_test_split
-from data_gen import *
+import pandas as pd
+from matplotlib import pyplot as mpl
+
+
+def plot_errors(df_errors, title):
+    clases = pd.unique(df_errors["Clase"])
+
+    colors = ["red", "blue", "green"]
+    linestyles = [":", "-.", "-"]
+
+    mpl.title(title)
+    for i in range(len(clases)):
+        df_clase = df_errors[df_errors["Clase"] == clases[i]]
+        mpl.plot(
+            df_clase["Épocas"],
+            df_clase["Error"],
+            color=colors[i],
+            label=clases[i],
+            linestyle=linestyles[i],
+        )
+
+    mpl.xlabel("Épocas")
+    mpl.ylabel("Errors")
+    mpl.legend()
+
+    mpl.show()
 
 
 def preprocess_cifar10_dataset():
@@ -19,9 +44,6 @@ def preprocess_cifar10_dataset():
 def ejercicio_1():
     X_raw, y_raw, X_test, y_test = preprocess_cifar10_dataset()
     X_train, X_val, y_train, y_val = train_test_split(X_raw, y_raw, random_state=0, test_size=0.2)
-
-    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
     
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'))
@@ -31,13 +53,13 @@ def ejercicio_1():
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
     model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(layers.Flatten())
+    model.add(layers.Flatten()) # Yo asumo que este Flatten es suficiente para reemplazar el que iria en preprocess. Creo que da decente.
     model.add(layers.Dense(64))
     model.add(layers.Dense(128))
     model.add(layers.Dense(128))
     model.add(layers.Dense(10))
 
-    epocas = 100
+    epocas = 30
 
     model.compile(optimizer='adam',
                 loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -59,3 +81,49 @@ def ejercicio_1():
     error_val = 1 - history.history['val_accuracy'][epocas - 1]
     error_test = 1 - test_acc
     print("Train Error: " + str(error_train) + " Val error: " + str(error_val) + " Test error: " + str(error_test) )
+
+def ejercicio_2():
+  X_raw, y_raw, X_test, y_test = preprocess_cifar10_dataset()
+  X_train, X_val, y_train, y_val = train_test_split(X_raw, y_raw, random_state=0, test_size=0.2)
+
+  errors = []
+  test_errors = []
+  epocas = 30 # No realicé el doble porque tomaba infinito
+  
+  for d1 in [0, 0.2, 0.5]:
+     for d2 in [0, 0.2, 0.5]:
+      model = models.Sequential()
+      model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3), padding='same'))
+      model.add(layers.MaxPooling2D((2, 2)))
+      model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+      model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+      model.add(layers.MaxPooling2D((2, 2)))
+      model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+      model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+      model.add(layers.Dropout(d1, seed=42)) # No se si sale bien el 42, pero al menos es consistente
+      model.add(layers.Flatten())
+      model.add(layers.Dense(64))
+      model.add(layers.Dropout(d2, seed=42))
+      model.add(layers.Dense(128))
+      model.add(layers.Dense(128))
+      model.add(layers.Dense(10))
+
+ 
+      model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+      history = model.fit(X_train, y_train, epochs=epocas, 
+                          validation_data=(X_val, y_val))
+      _, error_test = model.evaluate(X_test,  y_test, verbose=0)
+      test_errors.append([d1, d2, error_test])        
+
+      for i in range(epocas):
+          errors.append([1 - history.history['accuracy'][i], i + 1, "Train error on d1: " + str(d1) + ", d2: " + str(d2)])
+          errors.append([1 - history.history['val_accuracy'][i], i + 1, "Validation error on d1: " + str(d1) + ", d2: " + str(d2)])
+      print(errors)
+
+
+  df_errors = pd.DataFrame(errors, columns = ["Error", "Épocas", "Clase"])
+  df_test_errors = pd.DataFrame(errors, columns = ["d1", "d2", "Test error"])
+  print(df_test_errors)
+  plot_errors(df_errors, "Errores por época")
